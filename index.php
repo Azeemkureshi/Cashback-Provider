2db5d45bb415ffaede469904b63e8caaa3c547b1
Azeemkureshi <124749267+Azeemkureshi@users.noreply.github.com>
2024-09-16 07:00:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>Channel Name Camapign</title>
+<meta name="robots" content="noindex, follow">
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="icon" type="image/png" href=" http://earnnowcampaigns.rf.gd/IMG_20211019_203603_877.jpg"/>
+<link rel="stylesheet" href="https://CashbackBooster.in/css/style1.css">
+<link rel="stylesheet" href="https://CashbackBooster.in/css/promo1.css">
+<link href="https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap" rel="stylesheet">
+</head>
+
+<!-- Designing Started -->
+<style>
+.cta-btn {
+    background-color: rgb(69, 69, 185);
+    color: red;
+    padding: 18px 20px;
+    margin-top: 10px;
+    margin-bottom: 20px;
+    width: 100%;
+    border-radius: 10px;
+    border: none;
+}
+
+.btn-y{
+    font-weight:bold;
+    width: 55%;
+    background-image:linear-gradient(to right, #00c4cc,#00c4cc);
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+#telegram-image{
+        animation: continue 2s infinite;
+        animation-direction: alternate;
+    }
+
+@keyframes continue{
+        0%{
+            opacity: 0;
+        }
+        100%{
+            opacity: 1;
+        }
+    }
+*{font-family: 'Libre Baskerville', serif;}
+</style> 
+<!-- Designing Ended -->
+
+<body>
+    <section class="promo-main-section">
+        <div class="container">
+            <div class="promo-banner">
+
+                <div class="promo-banner-content text-white position-relative text-center">
+                    <div class="container">
+                        <div class="pro-banner-head">
+                            <!-- Enter Text You want -->
+                        <h2><font size='5.2px'><b>Instant Rs.100</b></font></h2>
+                          <h2 ><font size='3.2px'>Follow Stpes Given In Telegram $ At Bottom Of This Page</font></h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row justify-content-center">
+                <div class="col-lg-8 col-md-9">
+                    <div class="promo-offer-block rounded bg-white mb-4">
+                      <center>    <h3 class="promo-block-title"><b><font size='2px'>Must Join Telegram Channel For More Instant Payment Camapign. Thank You!!</font></b></h3></center>
+		
+<div class="container">
+<br>
+<div class="form-group row align-items-center mb-4 mt-2">
+                        <div class="col-lg-8">
+                            <div class="input-group promo-input">
+                                <input type="text" required id="UserPhoneNum" name="mo" class="form-control"
+                                    maxlength="10" minlength="10" placeholder="Enter Your Paytm Number">
+
+<!-- For Correct Paytm Number  -->
+<script>   
+$("#mob_frm").submit(function(e) {
+            e.preventDefault();
+            if(!$("#mobile").val().match("[0-9]{10}"))  {
+                alert("Please Enter 10 digit Paytm number");
+                return;
+            }  
+        });
+</script> 
+  
+<input type="hidden" value="Cashpower" name="oo">	
+ 
+   <div class="input-group-append">
+     <button class="input-group-text" onclick="redirect()" type="submit">Submit It</button>
+</div></div>
+</div></div>
+<script type="text/javascript">(function() {var script=document.createElement("script");
+script.type="text/javascript";
+script.async =true;
+script.src="//telegram.im/widget-button/index.php?id=Channel_Link";
+document.getElementsByTagName("head")[0].appendChild(script);})();
+</script>
+<a href="https://telegram.im/Channel_Link" target="_blank" class="telegramim_button telegramim_shadow" style="font-size:25px; width:316px; background:#ff00ff; box-shadow:1px 1px 5px #ff00ff; color:#d7fbfb; border-radius:30px;" title="Join Our Channel For Daily Cash"><i></i>Join For Paytm Cash</a>
+
+    <div class="promo-hr">
+        <div class="promo-hr-icon"></div>
+    </div>
+
+<script>
+function redirect(){
+    var number = document.getElementById("UserPhoneNum").value;
+    var currentUrl = window.location.href;
+    const params = new URL(currentUrl);
+    var encodedStringAtoB = params.searchParams.get("ref");
+  const verify = /^\d{10}$/;
+
+// Decode the String
+
+var decodedStringAtoB = atob(encodedStringAtoB);
+
+     if(verify.test(number))
+    {
+   var url ="https://earningzonepvtltd10921187.o18.link/c?o=16243570&m=8588&a=318083&aff_click_id=" + encodeURIComponent(number) + "&sub_aff_id="+ encodeURIComponent(decodedStringAtoB)+"";
+   
+    setTimeout(function(){ 
+    window.location = url; }, 2000);
+
+    }
+    else {
+        alert("Enter Correct Paytm Number");
+    }
+}
+
+</script>
+            <div class="my-4">
+                <h5 class="font-weight-bold mb-3">How to Get Instant Payment :</h5>
+                <div class="rounded bg-light text-muted p-3">
+                <div class="promo-list">
+                    <p><br></p>
+                </div>
+                <!-- Enter The Rules $ Steps To Be Followed --> 
+            <div class="promo-list">
+                    <p><span style="font-weight: bold;">
+                        1. Enter your Paytm number & Click Submit.<br><br>
+                        2. Wait For Few Seconds You'll Be Redirected To Partner Website<br>
+                        3. ⚠️ Follow All Stpes Given In Telegram<br><br>
+                         Done !! You Will Get Paytm Cash Instant If You Follow<br>
+                    </span><br></p>
+                            </div>
+                        </div>
+                    </div>
+
+            <div class="mb-4">
+                <h5 class="font-weight-bold  mb-3">Offer Terms & Conditions :</h5>
+                    <div class="rounded bg-light text-muted p-3">
+                        <div class="promo-list">
+                    <p>
+            <span style="color: Red; font-family: Times; font-size: 16px; font-weight: bold;">Offer is valid Only for New users but make sure to follow above steps. Paytm Cashback Will Credit Instant. Thank You!
+            </span><br></p>
+                </body>
+            <html>
+        </div>
+    </div>
+
+<center>
+       <div class="promo-hr">
+        <div class="promo-hr-icon"></div>
+    </div>
+        <span style="color: #000000; font-family: Times; font-size: 17px; font-weight: bold;">Thanks For Visiting Earn Rupees Campaign
+        </span>
+</center>
+</div> </div> </div>
+</div> </div> </div>
+</div> <br>
+<div class="col-md-12" style="position:fixed; bottom: 0; width: 100%; background: -webkit-linear-gradient(right, #de1afa,#26b9e4); right: 0; z-index:99;">
+<a href="http://telegram.dog/Channel_NaME_Here" style="text-decoration:underline;text-decoration-color:#fff;font-family: Poppins-Bold;" target="_blank" ><div  align="center" style="color: white;font-size:20px;    padding: 5px;"><img src="https://cashwala.in/images/telegram-512.png" style="width: 35px; margin-right: 5px;"> Join Telegram Channel</h3> </div>
+</div>
+</html>
